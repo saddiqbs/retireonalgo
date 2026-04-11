a0ce6795de32cb780ded66bad89e52859d1795e4
saddiqbs <saddiq.bello1@gmail.com>
2026-04-11 12:47:39 +0200

    Fix Pera Connect production build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ export default defineConfig({
   define: {
     global: 'globalThis',
   },
+  optimizeDeps: {
+    include: ['@perawallet/connect'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@perawallet/, /node_modules/],
+    },
+  },
+  resolve: {
+    alias: {
+      'js-sha3': 'js-sha3/src/sha3.js',
+    },
+  },
 })
